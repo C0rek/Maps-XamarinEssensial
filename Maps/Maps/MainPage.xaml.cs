cba690b6c093c28aa7df3c02138c9c95e7881d34
Tomas Rodriguez Zoni <corcho836@hotmail.com>
2023-10-18 16:04:05 -0300

    Add project files.

---
+++ Maps/Maps/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Maps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Maps_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(Latitudetxt.Text, out double lat))
+                return;
+            if (!double.TryParse(Longitudetxt.Text, out double lng))
+                return;
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions{
+                NavigationMode = NavigationMode.None
+            });
+            
+        }
+    }
+}
